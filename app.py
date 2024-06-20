d7a6b82c7525858b81eba6d3718c438d8089b48f
ooferdoodles <pantaisuyasri@gmail.com>
2024-06-20 18:40:14 +0700

    resized ui

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ with gr.Blocks() as demo:
     gr.Markdown("# ChangeChip")
     
     with gr.Row():
-        with gr.Column():
+        with gr.Column(scale=1):
             input_image = gr.Image(label="Input Image")
             reference_image = gr.Image(label="Reference Image")
             resize_factor = gr.Slider(0.1, 1, 0.5, step=0.1, label="Resize Factor")
             output_alpha = gr.Slider(0, 255, 50, step=1, label="Output Alpha")
         
-        with gr.Column():
+        with gr.Column(scale=2):
             output_image = gr.Image(label="Output Image")
             btn = gr.Button("Run")
     
